917e17352572006649defa12236887375d1fb9ae
Bhavishya <Bhavishya@DESKTOP-PDN1EFK>
2022-04-23 14:02:44 +0530

    Commiting master with changes

--- src/htakshi/Hitakshi1.java
+++ src/htakshi/Hitakshi1.java
@@ -5,6 +5,7 @@ public class Hitakshi1 {
 	public static void main(String[] args) {
 		
 		System.out.println("i am hitakshi from my end");
+		System.out.println("i am hitz from second end");
 	}
 
 }
